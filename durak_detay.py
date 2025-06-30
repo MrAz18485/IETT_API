b88ab69cf01560bce0424e08b50e3f66d19479ea
MrAz18485 <MrAz18485@hotmail.com>
2025-07-01 00:53:07 +0300

    Finished unit tests of durak_detay.py

--- durak_detay.py
+++ durak_detay.py
@@ -1,64 +1,91 @@
+import lxml.etree
 import zeep
 import sys
+import lxml
 
 import utils.functions
 
-wsdl = "xml/durak_hat_bilgi.xml"
+def take_inputs(): # For handling I/O
+    line_code = utils.functions.special_char_upper_func(input("Hat kodu giriniz / Enter bus code: "))
+    
+    if line_code == "":
+        raise ValueError("Hat kodu boş bırakılamaz / Bus code cannot be left empty")
+    
+    direction_choice = utils.functions.special_char_upper_func(input("Gitmek istediğiniz yönü giriniz (tüm yönler için boş bırakın) / Enter direction you would like to go (for all directions leave empty): "))
 
-try:
+    print("1 - Durak listele\n2 - Durak ara")
+    choice = input("Tercih giriniz / Enter choice: ")
+
+    if choice not in ["1", "2"]:
+        raise ValueError("Hatalı tercih / Invalid choice")
+    
+    stop_name = ""
+    if choice == "2":
+        stop_name = utils.functions.special_char_upper_func(input("Durak adı giriniz / Enter stop name: "))
+
+    IO_Dict = {"Line Code": line_code, "Direction": direction_choice, "Choice": choice, "Stop": stop_name}
+    return IO_Dict
+
+def soap_call(hat_kodu, wsdl):
     client = zeep.Client(wsdl=wsdl)
-
-    hat_kodu = utils.functions.special_char_upper_func(input("Hat kodu giriniz / Enter bus code: "))
-    
-    if hat_kodu == "":
-        raise Exception("Hat kodu boş bırakılamaz / Bus code cannot be left empty")
-    
     root = client.service.DurakDetay_GYY_wYonAdi(hat_kodu)
 
     if len(root) == 0:
         print("Hat bulunamadı / Bus line not found")
         exit()
-        
-    print("1 - Durak listele\n2 - Durak ara")
-    choice = input("Tercih giriniz / Enter choice: ")
+    return root
 
-    if int(choice) != 1 and int(choice) != 2:
-        raise Exception("Hatalı tercih / Invalid choice")
-
-    direction_choice = utils.functions.special_char_upper_func(input("Gitmek istediğiniz yönü giriniz (tüm yönler için boş bırakın) / Enter direction you would like to go (for all directions leave empty): "))
+def parse_soap_response(inputs, root):
     outp_buffer = []
-
-    if (int(choice) == 1):
-        if direction_choice == "": # prints all stops 
+    if inputs["Choice"] == "1":
+        if inputs["Direction"] == "":
             for table in root:
                 outp_buffer.append(table)
         else:
             for table in root:
-                if direction_choice in table[2].text: # display only the tables that have chosen direction
+                if inputs["Direction"] in table[2].text:
                     outp_buffer.append(table)
-    elif (int(choice) == 2):
-        durak_adi = utils.functions.special_char_upper_func(input("Durak adı giriniz / Enter stop name: "))
-        if direction_choice == "":
+
+    elif inputs["Choice"] == "2":
+        if inputs["Direction"] == "":
             for table in root:
-                if durak_adi in table[5].text:
+                if inputs["Stop"] in table[5].text:
                     outp_buffer.append(table)
         else:
             for table in root:
-                if direction_choice in table[2].text and durak_adi in table[5].text:
+                if inputs["Direction"] in table[2].text and inputs["Stop"] in table[5].text:
                     outp_buffer.append(table)
     else:
-        raise Exception("Hatalı tercih / Invalid choice")
+        raise ValueError("Hatalı tercih / Invalid choice")
     
-    if len(outp_buffer) == 0:
+    if len(outp_buffer) == 0: # I prefer not raising exceptions for this case, this can be a totally valid case.
         print("Durak bulunamadı / No stop found")
         exit(1)
-    
-    print()
-    for table in outp_buffer:
+
+    return outp_buffer
+
+def print_xml_tree_tables(input_tree):
+    print() # for better styling
+    for table in input_tree:
         for element in table.iterchildren():
-            print(element.tag, ":", element.text) # display the elements in buffer
+            print(element.tag, ":", element.text) # display the elements in current table
         print()
 
+def main():
+    try:
+        wsdl = "xml/durak_hat_bilgi.xml"
 
-except Exception as exc:
-    print("An exception occurred:", exc)
+        inputs = take_inputs()
+        
+        root = soap_call(inputs["Line Code"], wsdl)
+        print(lxml.etree.tostring(root))
+
+        outp_buffer = parse_soap_response(inputs, root)
+        
+        print_xml_tree_tables(outp_buffer)
+
+    except ValueError as val_exc:
+        print("Value error exception occurred:", val_exc)
+
+if __name__ == "__main__":
+    main()
